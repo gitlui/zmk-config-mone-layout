4987cec5bea8484d00f3c1e4ae3cabe7a6c24036
gitlui <l.goebkes@gmail.com>
2023-02-14 09:31:42 +0100

    First state of mone layout transfered from qmk to zmk

---
+++ config/german_aliases.h
@@ -0,0 +1,71 @@
+#define DE_A        A                  // A
+#define DE_B        B                  // B
+#define DE_C        C                  // C
+#define DE_D        D                  // D
+#define DE_E        E                  // E
+#define DE_F        F                  // F
+#define DE_G        G                  // G
+#define DE_H        H                  // H
+#define DE_I        I                  // I
+#define DE_J        J                  // J
+#define DE_K        K                  // K
+#define DE_L        L                  // L
+#define DE_M        M                  // M
+#define DE_N        N                  // N
+#define DE_O        O                  // O
+#define DE_P        P                  // P
+#define DE_Q        Q                  // Q
+#define DE_R        R                  // R
+#define DE_S        S                  // S
+#define DE_T        T                  // T
+#define DE_U        U                  // U
+#define DE_V        V                  // V
+#define DE_W        W                  // W
+#define DE_X        X                  // X
+#define DE_Y        Z                  // Y
+#define DE_Z        Y                  // Z
+
+#define DE_DOT      PERIOD             // .
+#define DE_COMM     COMMA              // ,
+#define DE_MINS     SLASH              // -
+
+#define DE_AE       SINGLE_QUOTE       // Ä
+#define DE_OE       SEMICOLON          // Ö
+#define DE_UE       LEFT_BRACKET       // Ü
+#define DE_SS       MINUS              // ß
+
+#define DE_CIRC     GRAVE             // ^ (dead)
+#define DE_EXLM     LS(NUMBER_1)      // !
+#define DE_DQUO     LS(NUMBER_2)      // "
+#define DE_DLR      LS(NUMBER_4)      // $
+#define DE_AMPR     LS(NUMBER_6)      // &
+#define DE_PERC     LS(NUMBER_5)      // %
+
+#define DE_PIPE     RA(DE_LABK)       // |
+#define DE_AT       RA(DE_Q)          // @
+#define DE_TILD     RA(DE_PLUS)       // ~
+#define DE_EURO     RA(DE_E)          // €
+#define DE_PLUS     RIGHT_BRACKET     // +
+#define DE_ASTR     LS(DE_PLUS)       // *
+#define DE_DEG      LS(DE_CIRC)       // °
+#define DE_SECT     LS(NUMBER_3)      // §
+
+#define DE_LPRN     LS(NUMBER_8)      // (
+#define DE_RPRN     LS(NUMBER_9)      // )
+#define DE_LCBR     RA(NUMBER_7)      // {
+#define DE_RCBR     RA(NUMBER_0)      // }
+#define DE_LBRC     RA(NUMBER_8)      // [
+#define DE_RBRC     RA(NUMBER_9)      // ]
+#define DE_LABK     NON_US_BACKSLASH  // <
+#define DE_RABK     LS(DE_LABK)       // >
+#define DE_ACUT     EQUAL             // ´ (dead)
+#define DE_HASH     NON_US_HASH       // #
+#define DE_COLN     LS(DE_DOT)        // :
+#define DE_SLSH     LS(NUMBER_7)      // /
+#define DE_EQL      LS(NUMBER_0)      // =
+#define DE_QUES     LS(DE_SS)         // ?
+#define DE_GRV      LS(DE_ACUT)       // ` (dead)
+#define DE_QUOT     LS(DE_HASH)       // '
+#define DE_SCLN     LS(DE_COMM)       // ;
+#define DE_UNDS     LS(DE_MINS)       // _
+#define DE_BSLS     RA(DE_SS)         // (backslash)
